e3a660bc6bd843e36bf5313dc4e784c7e787a5e1
Otavio Araujo <otavio-luiz17@outlook.com>
2019-02-13 00:09:05 -0200

    Projeto retangulo

---
+++ Retangulo/Dados.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+namespace Retangulo
+{
+    class Dados
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+        
+    }
+}
---
+++ Retangulo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Retangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dados dados = new Dados();
+          
+            Console.WriteLine("Entre a largura e altura do rêtangulo: ");
+            dados.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            dados.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            
+            Console.WriteLine("AREA : " + dados.Area().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("PERIMETRO : " + dados.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("DIAGONAL : " + dados.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
